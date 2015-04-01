44020ea99859f7b77e8949c52b96e00ec03b3070
Melanie Dauber <mpdaube@emory.edu>
2015-04-01 16:13:23 -0400

    Create MovieInfo.java

---
+++ MovieInfo.java
@@ -0,0 +1,16 @@
+package hw5;
+
+public class MovieInfo {
+	String shortName;
+	String fullName;
+	int ID;
+	
+	public MovieInfo(int id, String s, String f) {
+		ID = id;
+		shortName = s;
+		fullName = f;
+	}
+
+}
+
+
